f764a42855f41f3559f00d5b5d2151c58ec74951
Michael Miltchev <34401309+mlmiltchev@users.noreply.github.com>
2019-09-24 22:08:59 -0500

    Revert "Sashi branch ics499"

--- scanner_releases_gantt.php
+++ scanner_releases_gantt.php
@@ -1,114 +1,125 @@
+<?php
+    $nav_selected = "SCANNER"; 
+    $left_buttons = "YES"; 
+    $left_selected = "RELEASESGANTT"; 
+
+    include("./nav.php");
+    global $db;
+?>
+
+<div class="right-content">
+    <div class="container">
+
+        <h3 style = "color: #01B0F1;">Scanner -> System Releases Gantt</h3>
+
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+            class Release{
+                constructor(id, name, type, status, open_date, dependency_date, freeze_date, rtm_date, manager, author, app_id){
+                    this.id = id;
+                    this.name = name;
+                    this.tpe = type;
+                    this.status = status;
+                    this.open_date = open_date;
+                    this.dependency_date = dependency_date;
+                    this.freeze_date = freeze_date;
+                    this.rtm_date = rtm_date;
+                    this.manager = manager;
+                    this.author = author;
+                    this.app_id = app_id;
+                }
+
+                getDataTableRow(){
+                    return [this.id, this.name, this.open_date, this.rtm_date, null, 100, null];
+                }
+            }
+
+            google.charts.load('current', {'packages':['gantt']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+                var data = new google.visualization.DataTable();
+
+                var rawData = [];
+
+                data.addColumn('string', 'Task ID');
+                data.addColumn('string', 'Task Name');
+                data.addColumn('date', 'Start Date');
+                data.addColumn('date', 'End Date');
+                data.addColumn('number', 'Duration');
+                data.addColumn('number', 'Percent Complete');
+                data.addColumn('string', 'Dependencies');
+
+                <?php
+                    $sql = "SELECT * from releases";
+                    $result = $db->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()) {
+
+                            //Changing this boolean value fufills requirement A3&A4.13
+                            $useDependencyDateInsteadOfOpenDate = false;
+
+                            $open =  $useDependencyDateInsteadOfOpenDate ? explode('-', $row["dependency_date"]) : explode('-', $row["open_date"]);
+                            $dependency = $useDependencyDateInsteadOfOpenDate ? explode('-', $row["open_date"]) : explode('-', $row["dependency_date"]);
+                            $freeze = explode('-', $row["freeze_date"]);
+                            $rtm = explode('-', $row["rtm_date"]);
+                            
+                            echo "rawData.push(new Release(";
+                            echo "'{$row["id"]}',";
+                            echo "'{$row["name"]}',";
+                            echo "'{$row["type"]}',";
+                            echo "'{$row["status"]}',";
+                            echo "new Date({$open[0]},{$open[1]}-1,{$open[2]}),";
+                            echo "new Date({$dependency[0]},{$dependency[1]}-1,{$dependency[2]}),";
+                            echo "new Date({$freeze[0]},{$freeze[1]}-1,{$freeze[2]}),";
+                            echo "new Date({$rtm[0]},{$rtm[1]}-1,{$rtm[2]}),";
+                            echo "'{$row["manager"]}',";
+                            echo "'{$row["author"]}',";
+                            echo "'{$row["app_id"]}',";
+                            echo "));\r\n";
+                        }
+                    }else {
+                        echo "0 results";
+                    }
+                    $result->close();
+                ?>
+
+                rawData.sort((a, b) => (a.open_date > b.open_date) ? 1 : -1);
+
+                rawData.forEach(function (element){data.addRow(element.getDataTableRow())});
+
+                var trackHeight = 30;
+
+                var options = {
+                    height: data.getNumberOfRows() * trackHeight + 50,
+                    width: 1200,
+                    gantt: {
+                        trackHeight: trackHeight,
+                        percentEnabled: false
+                    }
+                };
+
+                var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
+
+                chart.draw(data, options);
+            }
+        </script>
+
+        <div id="chart_div"></div>
+    </div>
+</div>
+
+<style>
+    tfoot {
+        display: table-header-group;
+    }
+    #chart_div{
+        overflow-x: scroll;
+        overflow-y: hidden;
+    }
+</style>
 
 <?php
-
-  $nav_selected = "SCANNER"; 
-  $left_buttons = "YES"; 
-  $left_selected = "RELEASESGANTT"; 
-  $null_variable = null;
-
-  include("./nav.php");
-  global $db;
-
-  ?> 
-
-<html>
-  <head>
-      <!--START OF GOOGLE GANTT CHART-->
-      
-      <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-      
-
-      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-      <script type="text/javascript">
-      google.charts.load('current', {'packages':['gantt']});
-      //google.charts.setOnLoadCallback(drawChart);
-      google.charts.setOnLoadCallback(drawChart);
-
-      function daysToMilliseconds(days) {
-        return days * 24 * 60 * 60 * 1000;
-      }     
-      
-
-      //BEGINNING OF drawChart()     
-      function drawChart() {
-
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Task ID');
-        data.addColumn('string', 'Task Name');
-        data.addColumn('string', 'Resource');
-        data.addColumn('date', 'Start Date');
-        data.addColumn('date', 'End Date');
-        data.addColumn('number', 'Duration');
-        data.addColumn('number', 'Percent Complete');
-        data.addColumn('string', 'Dependencies');       
-
-        data.addRows([
-          
-            <?php
-              $sql = "SELECT * from releases ORDER BY open_date ASC;";
-              $result = $db->query($sql);
-                           
-              if($result->num_rows > 0){
-                while($row = $result->fetch_assoc()){
-                    //Separating date to convert into comma separated date format
-                    $start_date = $row['open_date'];
-                    $start_date_values = preg_split("/\-/",$start_date);
-                    $start_date_with_comma = $start_date_values[0].",".$start_date_values[1].",".$start_date_values[2];
-
-                    $end_date = $row['rtm_date'];
-                    $end_date_values = preg_split("/\-/",$end_date);
-                    $end_date_with_comma = $end_date_values[0].",".$end_date_values[1].",".$end_date_values[2];
-                  
-                    echo "['".
-                      $row['id'].
-                      "','".
-                      $row['name'].   
-                      "','".                   
-                      $row['status'].
-                      "',new Date(".$start_date_with_comma."),new Date(".$end_date_with_comma."),".
-                      "null".
-                      ",".
-                      "0".
-                      ",".
-                      "null".
-                      ",".
-                      "],";
-                  }
-              }
-            ?>
-        ]);
-
-        var options = {
-          height:1000,
-        };
-
-        var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
-
-        chart.draw(data, options);
-      }
-      //END OF drawChart()
-      
-
-          
-    </script>
-  <!--END OF GOOGLE GANTT CHART-->
-  </head>
-
-  <body>
- 
-    <div class="right-content">
-        <!--<div class="container" id="gantt_div"></div>-->
-        <div class="container"></div>
-
-          <h3 style = "color: #01B0F1;">Scanner -> System Releases Gantt</h3>
-          <h3><img src="images/gantt.png" style="max-height: 35px;" />Releases Gantt Chart</h3>
-          <div id="chart_div" style="max-height:500px; max-width:auto; overflow-x:scroll;"></div>
-
-        
-    </div>
-  </body>
-</html>          
-
+include("./footer.php");
+?>
