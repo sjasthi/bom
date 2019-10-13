2998ff3e8be21529b9a791ff7562410ce5f7cb18
Sashi Raj Amatya <ae0787sc@metrostate.edu>
2019-10-13 08:56:46 -0500

    added accordion style menu to select between difeerent charts.
    added accordion style menu to select between difeerent charts.

--- reports_request_step_chart.php
+++ reports_request_step_chart.php
@@ -11,6 +11,11 @@
 <html>
 
     <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    
     </head>
 
     <body>
@@ -22,9 +27,41 @@
 
             </div>
         </div>  
-    
-        <h3>File for this page: reports_<span style="color:red">request_step</span>_chart.php</h3>
 
+        <br>
+        <div class="container">
+
+            <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse-pie-chart">Pie Chart</a>
+                    </h4>
+                </div>
+                <div id="collapse-pie-chart" class="panel-collapse collapse in">
+                    <div class="panel-body">
+                        <h3>Display pie-chart here!</h3>
+                    </div>
+                </div>
+                </div>
+
+                <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse-bar-chart">Bar Chart</a>
+                    </h4>
+                </div>
+                <div id="collapse-bar-chart" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <h3>Display bar-chart here!</h3>
+                    </div>
+                </div>
+                </div>               
+                
+            </div> 
+        </div>
+
+        <h3> &nbsp; &nbsp; &nbsp; &nbsp; File for this page: reports_<span style="color:red">request_step</span>_chart.php</h3>
     </body>
 
 </html>
