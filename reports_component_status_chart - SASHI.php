3eecb34c74bb81e6f967135eeb7f0d7ab8c7c6b3
Sashi Raj Amatya <ae0787sc@metrostate.edu>
2019-10-20 14:27:16 -0500

    For Katrina to look at my code
    - Accordion menu shows in collapsed mode
    - I need help to figure out how to convert javascript variable to PHP variable in line 73-75

---
+++ reports_component_status_chart - SASHI.php
@@ -0,0 +1,191 @@
+<?php
+
+    $nav_selected = "REPORTS"; 
+    $left_buttons = "YES"; 
+    $left_selected = "COMPONENTSTATUSCHART"; 
+
+    include("./nav.php");
+    global $db;
+   
+?>
+<html>
+
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>        
+
+
+       
+
+        <!--Google pie Chart Code
+        -->
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+
+                var data = google.visualization.arrayToDataTable([
+                    ['Component Status', 'Counts'],
+
+                    //Pulling data from database
+                    <?php
+                        $sql = "SELECT cmp_status, COUNT(cmp_status) AS counts FROM sbom GROUP BY cmp_status;";
+                        $result = $db->query($sql);
+
+                        if($result->num_rows > 0){
+                            while($row = $result->fetch_assoc()){
+                            echo "['".
+                            $row['cmp_status'].
+                            "',".
+                            $row['counts'].
+                            "],";
+                            }
+                        }
+                    ?>
+                    ]);
+
+                var options = {
+                    title: 'Component Status Pie Chart',
+                    width: 900,
+                    height: 500,
+                };
+
+
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+                google.visualization.events.addListener(chart, 'select', selectHandler);
+
+
+                
+                function selectHandler(){
+                    var selectedItem = chart.getSelection()[0];
+                   
+                        
+                    if (selectedItem) {
+                    var valueFromPieChartSelection = data.getValue(selectedItem.row, 0);   
+                    
+                    
+                    }    
+                    <?php                       
+                        $valueFromPieChartSelection = "<script>valueFromPieChartSelection</script>". " Sashi Amatya";                      
+                    ?>                 
+                }
+                chart.draw(data, options);
+            }            
+        </script>
+        
+
+
+        <!-- Google bar chart
+        -->
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+            google.charts.load('current', {packages: ['corechart', 'bar']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+                var data = google.visualization.arrayToDataTable([
+                    ['Component Status', 'Counts'],
+               
+                    <?php
+                            $sql = "SELECT cmp_status, COUNT(cmp_status) AS counts FROM sbom GROUP BY cmp_status;";
+                            $result = $db->query($sql);
+
+                            if($result->num_rows > 0){
+                                while($row = $result->fetch_assoc()){
+                                echo "['".
+                                $row['cmp_status'].
+                                "',".
+                                $row['counts'].
+                                "],";
+                                }
+                            }
+                        ?>
+
+                ]);
+
+                var options = {
+                    title: 'Component Report',
+                    width: 900,
+                    height: 500,
+                    hAxis: {
+                        title: 'Counts',
+                        minValue: 0
+                    },
+                    vAxis: {
+                        title: 'Component Status'
+                    },
+                    bars: 'horizontal'
+                };
+
+                var chart = new google.visualization.BarChart(document.getElementById('barchart'));
+                chart.draw(data, options);
+            }
+        </script>
+    
+    </head>
+
+    <body>
+        <div class="right-content">
+            <div class="container">
+
+                <h3 style = "color: #01B0F1;">Reports --> Component Status Chart.</h3>
+                <h3><img src="images/reports.png" style="max-height: 35px;" /> Component Status Chart</h3>
+
+            </div>
+        </div>  
+
+        <br>
+        <div class="container">
+            <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse-pie-chart">Pie Chart</a>
+                    </h4>
+                </div>
+                <div id="collapse-pie-chart" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div id="piechart" style="width: 900px; height: 500px;"></div>
+
+                        <?php      
+                                           
+                        echo "value = " . $valueFromPieChartSelection;
+                         ?>
+                   
+                    </div>
+                </div>
+                </div>
+
+                <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse-bar-chart">Bar Chart</a>
+                    </h4>
+                </div>
+                <div id="collapse-bar-chart" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div id="barchart" style="width: 900px; height: 500px;"></div>
+                    </div>
+                </div>
+                </div>               
+                
+            </div> 
+        </div>
+
+    </body>
+
+</html>
+        
+
+ <style>
+   tfoot {
+     display: table-header-group;
+   }
+ </style>
+
+  <?php include("./footer.php"); ?>
