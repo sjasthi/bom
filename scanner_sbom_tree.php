a58945986565d8510cc0f3be15e9772a557ffdcb
Katrina Brinatte <up5757oo@metrostate.edu>
2019-11-02 11:32:50 -0500

    Update scanner_sbom_tree.php

--- scanner_sbom_tree.php
+++ scanner_sbom_tree.php
@@ -192,7 +192,7 @@
         { 
           searching: false, 
           ordering:  false, 
-          //"info": false,
+          "info": false,
           "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
         });
 
