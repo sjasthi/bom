4382989543ce46fbff5d8a06c77df36607aaecdd
Jaclyn <44209123+JaclynCao@users.noreply.github.com>
2020-02-18 19:50:33 -0600

    Added ability for user BOM preference through cookies
    - Added ability to set & get BOM preferences through cookies
    - BOM checkboxes added to scanner_releases_list.php
    - Buttons to set & get BOM preferences in scanner_releases_list.php & scanner_sbom_list.php
    - Some general code formatting for scanner_releases_list.php & scanner_sbom_list.php

--- scanner_releases_list.php
+++ scanner_releases_list.php
@@ -6,134 +6,169 @@
 
   include("./nav.php");
   global $db;
+?>
+<?php
+  global $count_err;
+  /*----------------- SET PREFERENCE COOKIE -----------------*/
+  $cookie_name = 'preference';
+  $expire = strtotime('+1 year');
 
-  ?>
+  //Get selected apps & put into cookie
+  if(isset($_POST['save']) && isset($_POST['app'])) {
+    $apps = $_POST['app'];
 
+    if(count($apps) > 10) {
+      $count_err = "You can't select more than 10 BOMS."; 
+    }else {
+      echo '<p 
+        style="font-size: 2.5rem; 
+        text-align: center; 
+        background-color: green; 
+        color: white;">BOM preferences successfully saved.</p>';
+      $preference = $apps;
+      $set_pref = setcookie($cookie_name, json_encode($preference), $expire); 
+    }
 
-<div class="right-content">
+  }elseif(isset($_POST['save']) && !isset($_POST['app'])) {
+    if(!isset($apps)) {
+      $count_err = "Please select at least one BOM."; 
+    }
+  }
+?>
+
+<!-- Display error for preference form -->
+<?php echo '<p 
+  style="font-size: 2.5rem; 
+  text-align: center; 
+  background-color: red; 
+  color: white;">'.$count_err.'</p>'
+?>
+
+  <div class="right-content">
     <div class="container">
-
       <h3 style = "color: #01B0F1;">Scanner -> System Releases List</h3>
+      <h3><img src="images/releases.png" style="max-height: 35px;" />System Releases</h3>
 
-        <h3><img src="images/releases.png" style="max-height: 35px;" />System Releases</h3>
+      <!-- Form to set user preference -->
+      <form id='app-form' name='app-form' method='post' action=''>
+      <table id="info" cellpadding="0" cellspacing="0" border="0"
+        class="datatable table table-striped table-bordered datatable-style table-hover"
+        width="100%" style="width: 100px;">
+        <thead>
+          <tr id="table-first-row">
+            <th>Select App</th>
+            <th>Application ID</th>
+            <th>Release ID</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Status</th>
+            <th>Open Date</th>
+            <th>Dependency Date</th>
+            <th>Content Date</th>
+            <th>RTM Date(s)</th>
+            <th>Manager</th>
+            <th>Author</th>
+          </tr>
+        </thead>
 
-        <table id="info" cellpadding="0" cellspacing="0" border="0"
-            class="datatable table table-striped table-bordered datatable-style table-hover"
-            width="100%" style="width: 100px;">
-              <thead>
-                <tr id="table-first-row">
-                <th>Application ID</th>
-                <th>Release ID</th>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>Status</th>
-                        <th>Open Date</th>
-                        <th>Dependency Date</th>
-                        <th>Content Date</th>
-                        <th>RTM Date(s)</th>
-                        <th>Manager</th>
-                        <th>Author</th>
-                </tr>
-              </thead>
+        <tbody>
+        <?php
+          $sql = "SELECT * from releases ORDER BY rtm_date ASC;";
+          $result = $db->query($sql);
 
+          if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+              echo '<tr>';
+              $appName = str_replace(' ', '', $row["name"]);
+              $sql2 = "SELECT DISTINCT app_id as appID FROM (select distinct concat(TRIM(app_name), 
+                TRIM(app_version)) as name, app_id from sbom ) as subquery where name ='".$appName."' Limit 1;";
+              $result2 = $db->query($sql2);
+              
+              echo "<td><input type='checkbox' name='app[]' value='".$row['app_id']."'></td>";
+              echo "<td>".$row["app_id"]."</td>";
+              echo '<td>'.$row["id"].'</td>';
+              if ($result2->num_rows > 0) {
+                while($row2 = $result2->fetch_assoc()) {
+                  $id = $row2["appID"];
+                }
 
+                echo '<td><a href="scanner_sbom_tree.php?id='.$id.'">'.$row["name"].' </a> </span> </td>';
+              }//end if
+              else {
+                echo '<td>'.$row["name"].' </span> </td>';
 
-              <tbody>
-
-              <?php
-
-$sql = "SELECT * from releases ORDER BY rtm_date ASC;";
-$result = $db->query($sql);
-
-                if ($result->num_rows > 0) {
-                    // output data of each row
-                    while($row = $result->fetch_assoc()) {
-                        echo '<tr>';
-                        $appName = str_replace(' ', '', $row["name"]);
-                        
-                          $sql2 = "SELECT DISTINCT app_id as appID FROM (select distinct concat(TRIM(app_name), TRIM(app_version)) as name, app_id from sbom ) as subquery where name ='".$appName."' Limit 1;";
-                                $result2 = $db->query($sql2);
-
-                                echo "<td>".$row["app_id"]."</td>";
-                                echo '<td>'.$row["id"].'</td>';
-
-                                if ($result2->num_rows > 0) {
-                                  while($row2 = $result2->fetch_assoc()) {
-                                    $id = $row2["appID"];
-                                  }
-                                  echo '<td><a href="scanner_sbom_tree.php?id='.$id.'">'.$row["name"].' </a> </span> </td>';
-                                }//end if
-                                else {
-                                  echo '<td>'.$row["name"].' </span> </td>';
-                                }//end else
-
-                                echo '<td>'.$row["type"].'</td>
-                                <td>'.$row["status"].'</td>
-                                <td>'.$row["open_date"].' </span> </td>
-                                <td>'.$row["dependency_date"].'</td>
-                                <td>'.$row["freeze_date"].'</td>
-                                <td>'.$row["rtm_date"].' </span> </td>
-                                <td>'.$row["manager"].' </span> </td>
-                                <td>'.$row["author"].' </span> </td>';
-      
-                                  $result2->close();
-                                }
-                              }
-                              ?>
-
-              </tbody>
-              <tfoot>
-                <tr>
-                <th>Application ID</th>
-                <th>Release ID</th>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>Status</th>
-                        <th>Open Date</th>
-                        <th>Dependency Date</th>
-                        <th>Content Date</th>
-                        <th>RTM Date(s)</th>
-                        <th>Manager</th>
-                        <th>Author</th>
-                </tr>
-              </tfoot>
+              }//end else
+              echo '<td>'.$row["type"].'</td>
+                <td>'.$row["status"].'</td>
+                <td>'.$row["open_date"].' </span> </td>
+                <td>'.$row["dependency_date"].'</td>
+                <td>'.$row["freeze_date"].'</td>
+                <td>'.$row["rtm_date"].' </span> </td>
+                <td>'.$row["manager"].' </span> </td>
+                <td>'.$row["author"].' </span> </td>';
+                $result2->close();
+            }
+          }
+        ?>
+        </tbody>
+          <tfoot>
+            <tr>
+              <th>Select App</th>
+              <th>Application ID</th>
+              <th>Release ID</th>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Status</th>
+              <th>Open Date</th>
+              <th>Dependency Date</th>
+              <th>Content Date</th>
+              <th>RTM Date(s)</th>
+              <th>Manager</th>
+              <th>Author</th>
+            </tr>
+          </tfoot>
         </table>
-
+        <button type='submit' name='save' value='submit'
+        style='background: #01B0F1;
+          color: white;
+          border: none;
+          border-radius: 10px;
+          padding: 1rem;
+          margin-right: 1rem;'>Set My BOMS</button>
+        </form>
+        <!-- End preference form -->                       
 
         <script type="text/javascript" language="javascript">
-    $(document).ready( function () {
-        
-        $('#info').DataTable( {
+          $(document).ready( function () {
+          $('#info').DataTable( {
             dom: 'lfrtBip',
             buttons: [
                 'copy', 'excel', 'csv', 'pdf'
             ] }
-        );
+          );
 
-        $('#info thead tr').clone(true).appendTo( '#info thead' );
-        $('#info thead tr:eq(1) th').each( function (i) {
+          $('#info thead tr').clone(true).appendTo( '#info thead' );
+          $('#info thead tr:eq(1) th').each( function (i) {
             var title = $(this).text();
             $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-    
             $( 'input', this ).on( 'keyup change', function () {
-                if ( table.column(i).search() !== this.value ) {
-                    table
-                        .column(i)
-                        .search( this.value )
-                        .draw();
-                }
+              if ( table.column(i).search() !== this.value ) {
+                table
+                .column(i)
+                .search( this.value )
+                .draw();
+              }
             } );
-        } );
+          } );
     
-        var table = $('#info').DataTable( {
+          var table = $('#info').DataTable( {
             orderCellsTop: true,
             fixedHeader: true,
             retrieve: true
-        } );
-        
-    } );
-
-</script>
+          } );  
+          } );
+        </script>
 
         
 
--- scanner_sbom_list.php
+++ scanner_sbom_list.php
@@ -5,140 +5,207 @@
 
   include("./nav.php");
   
- ?>
+  //PDO connection
+  $servername = 'localhost';
+  $dbname = 'bom';
+  $username = 'root';
+  $password = '';
+  $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+?>
 
- <div class="right-content">
+<?php
+  $cookie_name = 'preference';
+  global $pref_err;
+
+  function getBoms($db) {
+    $sql = "SELECT * from sbom;";
+    $result = $db->query($sql);
+    
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo '<tr>
+          <td>'.$row["row_id"].'</td>
+          <td><a class="btn" href="scanner_sbom_tree.php?id='.$row["app_id"].'">'.$row["app_id"].' </a> </td>
+          <td>'.$row["app_name"].'</td>
+          <td>'.$row["app_version"].'</td>
+          <td>'.$row["cmp_id"].' </span> </td>
+          <td>'.$row["cmp_name"].'</td>
+          <td>'.$row["cmp_version"].'</td>
+          <td>'.$row["cmp_type"].' </span> </td>
+          <td>'.$row["app_status"].' </span> </td>
+          <td>'.$row["cmp_status"].' </span> </td>
+          <td>'.$row["request_id"].'</td>
+          <td>'.$row["request_date"].'</td>
+          <td>'.$row["request_status"].'</td>
+          <td>'.$row["request_step"].'</td>
+          <td>'.$row["notes"].' </span> </td>
+          <td>'.$row["requestor"].'</td>
+          <td>'.$row["color"].'</td>
+        </tr>';
+      }//end while
+    }//end if
+    else {
+      echo "0 results";
+    }//end else
+    $result->close();
+  }
+
+  if(isset($_POST['get']) && !isset($_COOKIE[$cookie_name])) {
+    $pref_err = "You don't have BOMS saved.";
+  }
+?>
+  
+<!-- Display error if user retrieves preferences w/o any cookies set-->
+<?php echo '<p 
+  style="font-size: 2.5rem; 
+  text-align: center; 
+  background-color: red; 
+  color: white;">'.$pref_err.'</p>'
+?>
+  <div class="right-content">
     <div class="container">
-
       <h3 style = "color: #01B0F1;">Scanner --> Software BOM </h3>
+      <!-- Form to retrieve user preference -->
+      <form id='get-form' name='get-form' method='post' action='' style='display: inline;'>
+        <button type='submit' name='get' value='submit'
+        style='background: #01B0F1;
+          color: white;
+          border: none;
+          border-radius: 10px;
+          padding: 1rem;
+          margin-right: 1rem;'>Show My BOMS</button>
+      </form>
+      <form id='getall-form' name='getall-form' method='post' action='' style='display: inline;'>
+        <button type='submit' name='getall' value='submit' 
+        style='background: #01B0F1;
+          color: white;
+          border: none;
+          border-radius: 10px;
+          padding: 1rem;'>Show All BOMS</button>
+      </form>
 
-        <h3><img src="images/sbom_list.png" style="max-height: 35px;" />BOM List</h3>
+      <h3><img src="images/sbom_list.png" style="max-height: 35px;" />BOM List</h3>
+      <table id="info" cellpadding="0" cellspacing="0" border="0"
+        class="datatable table table-striped table-bordered datatable-style table-hover"
+        width="100%" style="width: 100px;">
+        <thead>
+          <tr id="table-first-row">
+            <th>Row ID</th>
+            <th>App ID</th>
+            <th>App Name</th>
+            <th>App Version</th>
+            <th>CMP ID</th>
+            <th>CMP Name</th>
+            <th>CMP Version</th>
+            <th>CMP Type</th>
+            <th>App Status</th>
+            <th>CMP Status</th>
+            <th>Request ID</th>
+            <th>Request Date</th>
+            <th>Request Status</th>
+            <th>Request Step</th>                        
+            <th>Notes</th>
+            <th>Requestor</th>
+            <th>Color</th>
+          </tr>
+        </thead>
+      <tbody>
+      <?php
+        /*----------------- GET PREFERENCE COOKIE -----------------*/
+        //get user preferred BOMS 
+        if(isset($_POST['get']) && isset($_COOKIE[$cookie_name])) {
+          $prep = rtrim(str_repeat('?,', count(json_decode($_COOKIE[$cookie_name]))), ',');
+          $sql = 'SELECT * FROM sbom WHERE app_id IN ('.$prep.')';
+          $pref = $pdo->prepare($sql);
+          $pref->execute(json_decode($_COOKIE[$cookie_name]));
 
-        <table id="info" cellpadding="0" cellspacing="0" border="0"
-            class="datatable table table-striped table-bordered datatable-style table-hover"
-            width="100%" style="width: 100px;">
-              <thead>
-                <tr id="table-first-row">
-                        <th>Row ID</th>
-                        <th>App ID</th>
-                        <th>App Name</th>
-                        <th>App Version</th>
-                        <th>CMP ID</th>
-                        <th>CMP Name</th>
-                        <th>CMP Version</th>
-                        <th>CMP Type</th>
-                        <th>App Status</th>
-                        <th>CMP Status</th>
-                        <th>Request ID</th>
-                        <th>Request Date</th>
-                        <th>Request Status</th>
-                        <th>Request Step</th>                        
-                        <th>Notes</th>
-                        <th>Requestor</th>
-                        <th>Color</th>
-                </tr>
-              </thead>
-              <tbody>
+          while($row = $pref->fetch(PDO::FETCH_ASSOC)) {
+            echo '<tr>
+              <td>'.$row["row_id"].'</td>
+              <td><a class="btn" href="scanner_sbom_tree.php?id='.$row["app_id"].'">'.$row["app_id"].' </a> </td>
+              <td>'.$row["app_name"].'</td>
+              <td>'.$row["app_version"].'</td>
+              <td>'.$row["cmp_id"].' </span> </td>
+              <td>'.$row["cmp_name"].'</td>
+              <td>'.$row["cmp_version"].'</td>
+              <td>'.$row["cmp_type"].' </span> </td>
+              <td>'.$row["app_status"].' </span> </td>
+              <td>'.$row["cmp_status"].' </span> </td>
+              <td>'.$row["request_id"].'</td>
+              <td>'.$row["request_date"].'</td>
+              <td>'.$row["request_status"].'</td>
+              <td>'.$row["request_step"].'</td>
+              <td>'.$row["notes"].' </span> </td>
+              <td>'.$row["requestor"].'</td>
+              <td>'.$row["color"].'</td>
+            </tr>';
+          }
+        }elseif(isset($_POST['get']) && !isset($_COOKIE[$cookie_name])) {
+          $pref_err = "You don't have BOMS saved.";
+          getBoms($db);
+        }//get all BOMS
+        elseif(isset($_POST['getall'])) {
+          getBoms($db);
+        }//if no preference cookie is set show all BOMS
+        else { 
+          getBoms($db);
+        }
+      ?>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Row ID</th>
+          <th>App ID</th>
+          <th>App Name</th>
+          <th>App Version</th>
+          <th>CMP ID</th>
+          <th>CMP Name</th>
+          <th>CMP Version</th>
+          <th>CMP Type</th>
+          <th>App Status</th>
+          <th>CMP Status</th>
+          <th>Request ID</th>
+          <th>Request Date</th>
+          <th>Request Status</th>
+          <th>Request Step</th> 
+          <th>Notes</th>
+          <th>Requestor</th>
+          <th>Color</th>
+        </tr>
+      </tfoot>
+      </table>
 
-              <?php
-
-              $sql = "SELECT * from sbom;";
-              $result = $db->query($sql);
-
-                if ($result->num_rows > 0) {
-                    // output data of each row
-                    while($row = $result->fetch_assoc()) {
-                        echo '<tr>
-                                <td>'.$row["row_id"].'</td>
-                                <td><a class="btn" href="scanner_sbom_tree.php?id='.$row["app_id"].'">'.$row["app_id"].' </a> </td>
-                                <td>'.$row["app_name"].'</td>
-                                <td>'.$row["app_version"].'</td>
-                                <td>'.$row["cmp_id"].' </span> </td>
-                                <td>'.$row["cmp_name"].'</td>
-                                <td>'.$row["cmp_version"].'</td>
-                                <td>'.$row["cmp_type"].' </span> </td>
-                                <td>'.$row["app_status"].' </span> </td>
-                                <td>'.$row["cmp_status"].' </span> </td>
-                                <td>'.$row["request_id"].'</td>
-                                <td>'.$row["request_date"].'</td>
-                                <td>'.$row["request_status"].'</td>
-                                <td>'.$row["request_step"].'</td>
-                                <td>'.$row["notes"].' </span> </td>
-                                <td>'.$row["requestor"].'</td>
-                                <td>'.$row["color"].'</td>
-
-                            </tr>';
-                    }//end while
-                }//end if
-                else {
-                    echo "0 results";
-                }//end else
-
-                 $result->close();
-                ?>
-
-              </tbody>
-              <tfoot>
-                <tr>
-                        <th>Row ID</th>
-                        <th>App ID</th>
-                        <th>App Name</th>
-                        <th>App Version</th>
-                        <th>CMP ID</th>
-                        <th>CMP Name</th>
-                        <th>CMP Version</th>
-                        <th>CMP Type</th>
-                        <th>App Status</th>
-                        <th>CMP Status</th>
-                        <th>Request ID</th>
-                        <th>Request Date</th>
-                        <th>Request Status</th>
-                        <th>Request Step</th> 
-                        <th>Notes</th>
-                        <th>Requestor</th>
-                        <th>Color</th>
-                </tr>
-              </tfoot>
-        </table>
-
-
-        <script type="text/javascript" language="javascript">
+  <script type="text/javascript" language="javascript">
     $(document).ready( function () {
-        
-        $('#info').DataTable( {
-            dom: 'lfrtBip',
-            buttons: [
-                'copy', 'excel', 'csv', 'pdf'
-            ] }
-        );
-
-        $('#info thead tr').clone(true).appendTo( '#info thead' );
-        $('#info thead tr:eq(1) th').each( function (i) {
-            var title = $(this).text();
-            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-    
-            $( 'input', this ).on( 'keyup change', function () {
-                if ( table.column(i).search() !== this.value ) {
-                    table
-                        .column(i)
-                        .search( this.value )
-                        .draw();
-                }
-            } );
-        } );
-    
-        var table = $('#info').DataTable( {
-            orderCellsTop: true,
-            fixedHeader: true,
-            retrieve: true
-        } );
-        
+    $('#info').DataTable( {
+      dom: 'lfrtBip',
+      buttons: ['copy', 'excel', 'csv', 'pdf']
     } );
 
-</script>
-
-        
-
+    $('#info thead tr').clone(true).appendTo( '#info thead' );
+    $('#info thead tr:eq(1) th').each( function (i) {
+      var title = $(this).text();
+      $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+      
+      $( 'input', this ).on( 'keyup change', function () {
+        if ( table.column(i).search() !== this.value ) {
+          table
+          .column(i)
+          .search( this.value )
+          .draw();
+        }
+      } );
+    } );
+    
+      var table = $('#info').DataTable( {
+        orderCellsTop: true,
+        fixedHeader: true,
+        retrieve: true
+      } );
+    } );
+  </script>
+      
  <style>
    tfoot {
      display: table-header-group;
