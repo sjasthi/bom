a1e125cbc6aca811b5b05f152aaf9752be80505f
Mike Juarez-Sweeney <pi7365or@go.metrostate.edu>
2020-03-01 16:04:44 -0600

    Added file import html

--- left_menu_scanner.php
+++ left_menu_scanner.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <script src="jquery-3.2.1.min.js"></script>
-
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $("#frmCSVImport").on("submit", function () {
-
-                $("#response").attr("class", "");
-                $("#response").html("");
-                var fileType = ".csv";
-                var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + fileType + ")$");
-                if (!regex.test($("#file").val().toLowerCase())) {
-                    $("#response").addClass("error");
-                    $("#response").addClass("display-block");
-                    $("#response").html("Invalid File. Upload : <b>" + fileType + "</b> Files.");
-                    return false;
-                }
-                return true;
-            });
-        });
-    </script>
-</head>
-<body>
 <div id="menu-left">
 
     <a href="scanner_releases_list.php">
@@ -84,14 +58,6 @@
                 <br/>BOM Compare<br/></div>
         </a>
 
-    <div id="response" class="<?php if (!empty($type)) {
-        echo $type . " display-block";
-    } ?>"><?php if (!empty($message)) {
-            echo $message;
-        } ?></div>
-    <div class="menu-left-current-page">
-        <div class="row">
-
             <form class="form-horizontal" action="" method="post"
                   name="frmCSVImport" id="frmCSVImport" enctype="multipart/form-data">
                 <img src="./images/file.png" alt="oops">
@@ -111,64 +77,4 @@
 
     </div>
 </body>
-<?php
-$conn = mysqli_connect("localhost", "root", "", "bom");
 
-if (isset($_POST["import"])) {
-
-    $fileName = $_FILES["file"]["tmp_name"];
-
-    $sqlDelete = "DELETE FROM sbom";
-    mysqli_query($conn, $sqlDelete);
-
-    if ($_FILES["file"]["size"] > 0) {
-
-        $file = fopen($fileName, "r");
-
-        while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
-            $sqlInsert = "INSERT into sbom (row_id,
-											app_id,
-											app_name,
-											app_version,
-											cmp_id,
-											cmp_name,
-											cmp_version,
-											cmp_type,
-											app_status,
-											cmp_status,
-											request_id,
-											request_date,
-											request_status,
-											request_step,
-											notes,
-											requester,
-											color)
-                   values ('" . $column[0] . "',
-                           '" . $column[1] . "',
-                           '" . $column[2] . "',
-                           '" . $column[3] . "',
-                           '" . $column[5] . "',
-						   '" . $column[6] . "',
-						   '" . $column[7] . "',
-						   '" . $column[8] . "',
-						   '" . $column[9] . "',
-						   '" . $column[10] . "',
-						   '" . $column[11] . "',
-						   '" . $column[12] . "',
-						   '" . $column[13] . "',
-						   '" . $column[14] . "',
-						   '" . $column[15] . "',
-						   '" . $column[16] . "'
-                           )";
-            $result = mysqli_query($conn, $sqlInsert);
-
-            if (!empty($result)) {
-                $type = "success";
-                $message = "CSV Data Imported into the Database";
-            } else {
-                $type = "error";
-                $message = "Problem in Importing CSV Data";
-            }
-        }
-    }
-}
