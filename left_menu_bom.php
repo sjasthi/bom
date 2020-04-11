b31d2cf600ab4cfbb0bfc5dfdfc607f569e97743
Jaclyn <44209123+JaclynCao@users.noreply.github.com>
2020-04-10 21:40:57 -0500

    minor change: renamed two files

--- left_menu_bom.php
+++ left_menu_bom.php
@@ -30,14 +30,14 @@
             <br/>BOM Tree V2<br/></div>
     </a>
 
-    <a href="reports_pieChart.php">
+    <a href="bom_pieChart.php">
       	<div <?php if($left_selected == "REPORTSPIECHART")
       	{ echo 'class="menu-left-current-page"'; } ?>>
       	<img src="./images/reports.png">
       	<br/>Pie Chart<br/></div>
     </a>
 
-    <a href="reports_barChart.php">
+    <a href="bom_barChart.php">
       	<div <?php if($left_selected == "REPORTSBARCHART")
       	{ echo 'class="menu-left-current-page"'; } ?>>
       	<img src="./images/reports.png">
